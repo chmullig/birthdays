77a1e78f5cb11ecef82a6fb7e05bbb3e7909190b
Chris Mulligan <chris.mulligan@gmail.com>
2012-06-12 15:09:13 -0400

    Updated per Gelman's comments at: 	http://andrewgelman.com/2012/06/simple-graph-win-the-example-of-birthday-frequencies/

--- birthdaygraph.R
+++ birthdaygraph.R
@@ -20,26 +20,31 @@ bdata$flag[bdata$month==2 & bdata$day==29] <- 4
 
 #plot
 png(file="birthdays.png", width=768, height=768)
-plot(births ~ order, data=bdata, xaxt="n", xlab="Day", ylab="Births", main="Births by Day of Year", type="l", lwd=0, cex=2)
-axis(1, at=by(bdata$order, list(bdata$month), min),
-    labels=c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"),
+plot(c(-366+bdata$order[bdata$month==12], bdata$order, 366+bdata$order[bdata$month==1]),
+    c(bdata$births[bdata$month==12]/mean(bdata$births)*100, bdata$births/mean(bdata$births)*100, bdata$births[bdata$month==1]/mean(bdata$births)*100),
+    xaxs="i", xaxt="n", xlab="Day", ylab="Births (% of average)", main="Births by Day of Year", type="l", lwd=0, cex=2)
+    
+axis(1, at=c(-30, by(bdata$order, list(bdata$month), min), 367),
+    labels=c("Dec", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec", "Jan"),
     tck=1, lwd=0.5, lty=2, col="grey", cex.axis=0.99)
 title(sub="Source: National Vital Statistics System natality data 1969-1988, as provided by Google BigQuery.  Graph by Chris Mulligan (chmullig.com)", cex.sub=1, adj=0.75)
 
+#shade months 0 and 13 
+rect(-30, 0, 0, 200, col = "grey85", lwd=0)
+rect(366, 0, 396, 200, col = "grey85", lwd=0)
+
 #add the mean line
-abline(a=sum(bdata$births)/365.25, b=0, col="red", lwd=0.5)
+abline(a=100, b=0, col="grey20", lwd=0.5)
 
-#add the smoothed line
-lines(bdata$order, bdata$predict, col="blue", lwd=0.5)
-
-legend("topleft", c("Births", "Smoothed", "Mean"), col=c("black", "blue", "red"), lty=1, lwd=2, bty="n")
 
 #label some outliers
-with(bdata, text(order[flag==1], births[flag==1], paste(month[flag==1], "/", day[flag==1]), pos=1, cex=1))
-with(bdata, text(order[flag==3], births[flag==3], paste(month[flag==3], "/", day[flag==3]), pos=3, cex=1))
-with(bdata, text(order[flag==4], births[flag==4], paste(month[flag==4], " / ", day[flag==4], "*4", sep=""), pos=3, cex=1, offset=-1))
+with(bdata, text(order[flag==1], (births[flag==1]/mean(births))*100, paste(month[flag==1], "/", day[flag==1]), pos=1, cex=1))
+with(bdata, text(order[flag==3], (births[flag==3]/mean(births))*100, paste(month[flag==3], "/", day[flag==3]), pos=3, cex=1))
+with(bdata, text(order[flag==4], (births[flag==4]/mean(births))*100, paste(month[flag==4], " / ", day[flag==4], "*4", sep=""), pos=3, cex=1, offset=-1))
 
 #write the actual line
-lines(births ~ order, data=bdata, xaxt="n", xlab="Day", ylab="Births", type="l", lwd=1.5)
+lines(c(-366+bdata$order[bdata$month==12], bdata$order, 366+bdata$order[bdata$month==1]),
+    c(bdata$births[bdata$month==12]/mean(bdata$births)*100, bdata$births/mean(bdata$births)*100, bdata$births[bdata$month==1]/mean(bdata$births)*100),
+    xaxt="n", xlab="Day", ylab="Births", type="l", lwd=1.5)
 
 dev.off()

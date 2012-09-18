34424cc14910e6e70f173cb5b38613b27d317e3e
Chris Mulligan <chris.mulligan@gmail.com>
2012-09-18 17:37:42 -0400

    EPS output

--- birthdaygraph.R
+++ birthdaygraph.R
@@ -19,7 +19,13 @@ bdata$flag[bdata$month==10 & bdata$day==31] <- 1
 bdata$flag[bdata$month==2 & bdata$day==29] <- 4
 
 #plot
-png(file="birthdays.png", width=768, height=768)
+#png(file="birthdays.png", width=768, height=768)
+#png(file="birthdays_hq.png", width=4267, height=4267, res=400)
+#pdf(file="birthdays_hq.pdf", pointsize=10, height=10, width=10)
+
+setEPS()
+postscript("birthdaygraph.eps", width=6, height=6, pointsize=9)
+
 plot(c(-366+bdata$order[bdata$month==12], bdata$order, 366+bdata$order[bdata$month==1]),
     c(bdata$births[bdata$month==12]/mean(bdata$births)*100, bdata$births/mean(bdata$births)*100, bdata$births[bdata$month==1]/mean(bdata$births)*100),
     xaxs="i", xaxt="n", xlab="Day", ylab="Births (% of average)", main="Births by Day of Year", type="l", lwd=0, cex=2)
@@ -27,7 +33,7 @@ plot(c(-366+bdata$order[bdata$month==12], bdata$order, 366+bdata$order[bdata$mon
 axis(1, at=c(-30, by(bdata$order, list(bdata$month), min), 367),
     labels=c("Dec", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec", "Jan"),
     tck=1, lwd=0.5, lty=2, col="grey", cex.axis=0.99)
-title(sub="Source: National Vital Statistics System natality data 1969-1988, as provided by Google BigQuery.  Graph by Chris Mulligan (chmullig.com)", cex.sub=1, adj=0.75)
+title(sub="Source: National Vital Statistics System natality data 1969-1988, as provided by Google BigQuery.  Graph by Chris Mulligan (chmullig.com)", cex.sub=.7, adj=0.5)
 
 #shade months 0 and 13 
 rect(-30, 0, 1, 200, col = "grey90", lwd=0)
